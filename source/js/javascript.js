d32e075fbdac858198c5597f2cb8fd45f0c79602
Денис Дьяконов <holosty@yandex.ru>
2021-05-22 22:25:57 +0300

    index.html all

--- source/js/javascript.js
+++ source/js/javascript.js
@@ -32,3 +32,17 @@ function initMap() {
     infowindow.open(map, marker);
   });
 }
+
+// Menu
+
+const nav = document.querySelector(".nav");
+const navBtn = nav.querySelector(".nav__btn");
+
+// Menu no JS
+nav.classList.remove("nav--no-js");
+
+// Menu tooggle
+navBtn.onclick = function () {
+  nav.classList.toggle("nav--close");
+  nav.classList.toggle("nav--open");
+}
